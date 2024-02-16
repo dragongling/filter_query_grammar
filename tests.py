7345812ce8242b17a1dc478c0da0c914e7500cf0
Alexander Slesarenko <dragongling@gmail.com>
2024-02-16 10:45:06 +0500

    seems to work

---
+++ tests.py
@@ -0,0 +1,25 @@
+import unittest
+
+from lark import Lark
+
+from main import DjangoFilterTransformer2
+
+
+class TestLarkParser(unittest.TestCase):
+    def setUp(self):
+        with open("grammar.enbf", "r") as file:
+            grammar = file.read()
+
+        self.parser = Lark(grammar, parser="lalr")
+
+    def test1(self):
+        text = "assignee=1"
+        tree = self.parser.parse(text)
+        res = DjangoFilterTransformer2().transform(tree)
+        self.assertEqual(str(res), "(AND: ('assignee', 1))")
+
+    def test2(self):
+        text = "assignee=1|position=1"
+        tree = self.parser.parse(text)
+        res = DjangoFilterTransformer2().transform(tree)
+        self.assertEqual(str(res), "(OR: ('assignee', 1), ('position', 1))")
